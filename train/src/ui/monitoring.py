19433cf44f8806e93295d4973baad3b14f31365e
cxnt <pavelbartsits@gmail.com>
2024-06-06 18:18:44 +0400

    update train

--- train/src/ui/monitoring.py
+++ train/src/ui/monitoring.py
@@ -195,7 +195,7 @@ def upload_artifacts_and_log_progress(task_type: str):
         app_name=g.checkpoint.app_name,
         session_id=g.task_id,
         session_path=remote_artifacts_dir,
-        weights_dir=remote_weights_dir,
+        weeights_path=remote_weights_dir,
         training_project_name=g.project_info.name,
         task_type=task_type,
         config_path=remote_config_path,
